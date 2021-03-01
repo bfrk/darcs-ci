7b29f367b1131d1bf54e19bf279f29053e06754f
Ben Franksen <ben.franksen@online.de>
2021-03-01 14:47:54 +0100

    snapshot

---
+++ src/Darcs/Repository/InternalTypes.hs
@@ -0,0 +1,114 @@
+-- Copyright (C) 2006-2007 David Roundy
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2, or (at your option)
+-- any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software Foundation,
+-- Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+module Darcs.Repository.InternalTypes
+    ( Repository
+    , PristineType(..)
+    , AccessType(..)
+    , SAccessType(..)
+    , repoAccessType
+    , repoCache
+    , modifyCache
+    , repoFormat
+    , repoLocation
+    , withRepoDir
+    , repoPristineType
+    , unsafeCoerceRepoType
+    , unsafeCoercePatchType
+    , unsafeCoerceR
+    , unsafeCoerceU
+    , unsafeCoerceT
+    , unsafeEndTransaction
+    , unsafeStartTransaction
+    , mkRepo
+    ) where
+
+import Darcs.Prelude
+
+import Darcs.Util.Cache ( Cache )
+import Darcs.Repository.Format ( RepoFormat )
+import Darcs.Util.File ( withCurrentDirectory )
+import Darcs.Util.Path ( AbsoluteOrRemotePath, toPath )
+import Unsafe.Coerce ( unsafeCoerce )
+
+data PristineType
+  = NoPristine
+  | PlainPristine
+  | HashedPristine
+    deriving ( Show, Eq )
+
+data AccessType = RO | RW deriving (Eq)
+
+data SAccessType (rt :: AccessType) where
+  SRO :: SAccessType 'RO
+  SRW :: SAccessType 'RW
+
+-- |A @Repository@ is a token representing the state of a repository on disk.
+-- It is parameterized by the patch type in the repository, and witnesses for
+-- the recorded state of the repository (i.e. what darcs get would retrieve),
+-- the unrecorded state (what's in the working tree now),
+-- and the tentative state, which represents work in progress that will
+-- eventually become the new recorded state unless something goes wrong.
+data Repository (rt :: AccessType) (p :: * -> * -> *) wRecordedstate wUnrecordedstate wTentativestate =
+  Repo !String !RepoFormat !PristineType Cache (SAccessType rt)
+
+type role Repository nominal nominal nominal nominal nominal
+
+repoLocation :: Repository rt p wR wU wT -> String
+repoLocation (Repo loc _ _ _ _) = loc
+
+-- | Perform an action with the current working directory set to the
+-- 'repoLocation'.
+withRepoDir :: Repository rt p wR wU wT -> IO a -> IO a
+withRepoDir repo = withCurrentDirectory (repoLocation repo)
+
+repoFormat :: Repository rt p wR wU wT -> RepoFormat
+repoFormat (Repo _ fmt _ _ _) = fmt
+
+repoPristineType :: Repository rt p wR wU wT -> PristineType
+repoPristineType (Repo _ _ pr _ _) = pr
+
+repoCache :: Repository rt p wR wU wT -> Cache
+repoCache (Repo _ _ _ c _) = c
+
+modifyCache :: (Cache -> Cache) -> Repository rt p wR wU wT -> Repository rt p wR wU wT
+modifyCache g (Repo l f p c a) = Repo l f p (g c) a
+
+repoAccessType :: Repository rt p wR wU wT -> SAccessType rt
+repoAccessType (Repo _ _ _ _ s) = s
+
+unsafeCoerceRepoType :: Repository rt p wR wU wT -> Repository rt' p wR wU wT
+unsafeCoerceRepoType = unsafeCoerce
+
+unsafeCoercePatchType :: Repository rt p wR wU wT -> Repository rt p' wR wU wT
+unsafeCoercePatchType = unsafeCoerce
+
+unsafeCoerceR :: Repository rt p wR wU wT -> Repository rt p wR' wU wT
+unsafeCoerceR = unsafeCoerce
+
+unsafeCoerceU :: Repository rt p wR wU wT -> Repository rt p wR wU' wT
+unsafeCoerceU = unsafeCoerce
+
+unsafeCoerceT :: Repository rt p wR wU wT -> Repository rt p wR wU wT'
+unsafeCoerceT = unsafeCoerce
+
+unsafeStartTransaction :: Repository 'RO p wR wU wT -> Repository 'RW p wR wU wT
+unsafeStartTransaction (Repo l f p c SRO) = Repo l f p c SRW
+
+unsafeEndTransaction :: Repository 'RW p wR wU wT -> Repository 'RO p wR wU wT
+unsafeEndTransaction (Repo l f p c SRW) = Repo l f p c SRO
+
+mkRepo :: AbsoluteOrRemotePath -> RepoFormat -> PristineType -> Cache -> Repository 'RO p wR wU wT
+mkRepo p f pr c = Repo (toPath p) f pr c SRO
---
+++ tests/list-options.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# some tests for the --list-options option
+
+. lib
+
+rm -rf R
+darcs init R
+cd R
+echo aboringfile > _darcs/prefs/boring
+touch anunaddedfile
+touch aboringfile
+darcs record --list-options | not grep -w anunaddedfile
+darcs record -l --list-options | grep -w anunaddedfile
+darcs record -l --list-options | not grep -w aboringfile
+darcs record -l --boring --list-options | grep -w anunaddedfile
+darcs record -l --boring --list-options | grep -w aboringfile
+darcs add --list-options | grep -w anunaddedfile
+darcs add --list-options | not grep -w aboringfile
+darcs add --boring --list-options | grep -w anunaddedfile
+darcs add --boring --list-options | grep -w aboringfile
+cd ..
