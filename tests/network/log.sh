5d1bfa1276a4e17cec889021aa7c8770917b00fb
Ben Franksen <ben.franksen@online.de>
2022-11-22 09:46:16 +0100

    tosend 2022-11-22 08:46:16+00:00

--- tests/network/log.sh
+++ tests/network/log.sh
@@ -12,12 +12,16 @@ darcs log --repo=http://darcs.net GNUmakefile --last 30
 
 # no _darcs should remain
 test ! -d _darcs
+
 # go to a directory where we have no write access
-# (I dearly hope nobody tries to run the tests as root!)
-cd /
+trap "chmod u+w $PWD/ro" EXIT
+mkdir ro
+chmod a-w ro
+cd ro
 # and try again (with less patches to fetch)
 darcs log --repo=http://darcs.net GNUmakefile --last 3
 # an absolute path should give an error
 not darcs log --repo=http://darcs.net /GNUmakefile --last 3
 # also test that it works without any filename arguments
 darcs log --repo=http://darcs.net --last 1
+cd ..
