2338868517bbe7cfb295f3800334cd6d6311b354
Ben Franksen <ben.franksen@online.de>
2022-11-23 08:40:59 +0100

    tosend 2022-11-23 07:40:59+00:00

--- tests/network/log.sh
+++ tests/network/log.sh
@@ -3,25 +3,28 @@
 . lib
 . httplib
 
-check_remote_http https://hub.darcs.net/darcs/darcs-screened
+check_remote_http http://darcs.net
 
 # Demonstrates issue385 and others
-darcs log --repo=https://hub.darcs.net/darcs/darcs-screened GNUmakefile --last 30
+darcs log --repo=http://darcs.net GNUmakefile --last 30
 
 # Test things mentioned in issue2461:
 
 # no _darcs should remain
 test ! -d _darcs
 
+sleep 1
 # go to a directory where we have no write access
 trap "chmod u+w $PWD/ro" EXIT
 mkdir ro
 chmod a-w ro
 cd ro
 # and try again (with less patches to fetch)
-darcs log --repo=https://hub.darcs.net/darcs/darcs-screened GNUmakefile --last 3
+darcs log --repo=http://darcs.net GNUmakefile --last 3
+sleep 1
 # an absolute path should give an error
-not darcs log --repo=https://hub.darcs.net/darcs/darcs-screened /GNUmakefile --last 3
+not darcs log --repo=http://darcs.net /GNUmakefile --last 3
+sleep 1
 # also test that it works without any filename arguments
-darcs log --repo=https://hub.darcs.net/darcs/darcs-screened --last 1
+darcs log --repo=http://darcs.net --last 1
 cd ..
