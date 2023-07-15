4d045b39762b4a92f919f6204108d568987e4468
Ben Franksen <ben.franksen@online.de>
2023-07-15 18:09:44 +0200

    tosend 2023-07-15 16:09:44+00:00

--- tests/issue612_repo_not_writable.sh
+++ tests/issue612_repo_not_writable.sh
@@ -14,22 +14,13 @@ darcs init
 touch t.t
 darcs add t.t
 darcs record -am "initial add"
-if [ -e _darcs/inventories ]; then
-  chmod a-w _darcs/inventories/*
-  chmod a-w _darcs/inventories
-fi
-if [ -e _darcs/inventory ]; then
-  chmod a-w _darcs/inventory
-fi
+trap "chmod -R +w $(pwd)/_darcs/inventories" EXIT
+chmod -R a-w _darcs/inventories
 cd ..
 
 darcs get temp1 temp2
 cd temp2
-# this block may fail so we'd better make sure we clean up after
-# ourselves to avoid a permissions mess for other tests
-trap "cd ..; chmod -R +w temp1" EXIT
 echo new >> t.t
 darcs record -am "new patch"
-not darcs push -a ../temp1 2> log
-grep failed log
-
+not darcs push -a ../temp1
+cd ..
