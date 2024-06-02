01b5bf2eaab8d1d2fdce071559899f3a026c34dc
Ganesh Sittampalam <hsenag@github.com>
2024-06-02 22:12:56 +0100

    screened 2024-06-02 21:12:56+00:00

--- src/Darcs/Prelude.hs
+++ src/Darcs/Prelude.hs
@@ -26,6 +26,7 @@ it to the Prelude import.
 module Darcs.Prelude
     ( module Prelude
     , module Control.Applicative
+    , module Data.List
     , module Data.Monoid
     , Semigroup(..)
     , module Data.Traversable
@@ -59,9 +60,14 @@ import Prelude hiding
     ,
       -- used by various code for no particularly good reason
       lookup, pred
+#if MIN_VERSION_base(4,20,0)
+    , -- because it's in the new Prelude but only in Data.List in older GHCs
+      foldl'
+#endif
     )
 
 import Control.Applicative ( Applicative(..), (<$>), (<*>) )
+import Data.List ( foldl' )
 import Data.Monoid ( Monoid(..) )
 import Data.Semigroup ( Semigroup(..) )
 import Data.Traversable ( traverse )
