8bba9a83ac15ebd41a3884d274192ab486091b85
Ben Franksen <Ben Franksen benjamin.franksen@helmholtz-berlin.de>
2024-08-12 16:09:59 +0200

    screened 2024-08-12 14:09:59+00:00

--- tests/issue2729-index-corner-case.sh
+++ tests/issue2729-index-corner-case.sh
@@ -8,7 +8,7 @@ darcs init R
 cd R
 
 # create a file with zero size and timestamp
-touch -d "1970-01-01 00:00:00 UTC" f
+touch -d "1970-01-01T00:00:00 UTC" f
 darcs record -lam 'add f'
 # this should not crash darcs:
 darcs unrecord -a
--- tests/issue2729-index-corner-case2.sh
+++ tests/issue2729-index-corner-case2.sh
@@ -11,7 +11,7 @@ touch b
 darcs record -lam 'add b'
 
 # create a file with zero size and timestamp
-touch -d "1970-01-01 00:00:00 UTC" f
+touch -d "1970-01-01T00:00:00 UTC" f
 darcs add f
 rm _darcs/index
 # this should not crash darcs:
